2f3c0158e8f405a5a31794d70747571269fb3897
Walber Jefferson <walberjefferson@hotmail.com>
2023-11-07 11:39:29 -0300

    Ajustando provider

--- src/NextCloudServiceProvider.php
+++ src/NextCloudServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace PWDev\NextCloudStorage;
 
+use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
+use League\Flysystem\WebDAV\WebDAVAdapter;
 use Sabre\DAV\Client;
 
 class NextCloudServiceProvider extends ServiceProvider
@@ -13,12 +15,12 @@ class NextCloudServiceProvider extends ServiceProvider
     {
         Storage::extend('nextcloud', function ($app, $config) {
 
-//            $pathPrefix = array_key_exists('pathPrefix', $config) ? $config['pathPrefix'] : null;
-            $pathPrefix = 'remote.php/dav/files/' . $config['userName'];
-            if (array_key_exists('pathPrefix', $config)) {
-                $pathPrefix = rtrim($config['pathPrefix'], '/') . '/' . $pathPrefix;
-            }
 
+//            $pathPrefix = 'remote.php/dav/files/' . $config['userName'];
+//            if (array_key_exists('pathPrefix', $config)) {
+//                $pathPrefix = rtrim($config['pathPrefix'], '/') . '/' . $pathPrefix;
+//            }
+            $pathPrefix = array_key_exists('pathPrefix', $config) ? $config['pathPrefix'] : null;
             $client = new Client([
                 'baseUri' => $config['baseUri'],
                 'userName' => $config['userName'],
@@ -26,9 +28,17 @@ class NextCloudServiceProvider extends ServiceProvider
             ]);
 
 //            $client = new Client($config);
-            $adapter = new NextCloudAdapter($client, $pathPrefix, $config);
+//            $adapter = new NextCloudAdapter($client, $pathPrefix, $config);
 
-            return new Filesystem($adapter);
+//            return new Filesystem($adapter);
+
+            $adapter = new WebDAVAdapter($client, $pathPrefix);
+
+            return new FilesystemAdapter(
+                new Filesystem($adapter, $config),
+                $adapter,
+                $config
+            );
         });
     }
 
